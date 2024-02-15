05c1f84984f42f060e7f9e0f6d35a9b91ff7ed4c
Casey <caseystephenabbott@gmail.com>
2024-02-14 21:06:50 -1000

    made every staff page. still missing some images

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const showMenu = (toggleId, navId, sectionId) => {
+const showMenu = (toggleId, navId) => {
     const toggle = document.getElementById(toggleId),
     nav = document.getElementById(navId)
 
@@ -8,3 +8,12 @@ const showMenu = (toggleId, navId, sectionId) => {
     })
 }
 showMenu('nav-toggler', 'nav-menu');
+
+
+const cards = document.querySelectorAll('.card');
+cards.forEach(card => {
+    card.addEventListener('click', () => {
+        card.classList.toggle('flip_card')
+    })
+});
+
