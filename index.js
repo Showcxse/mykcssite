28599f1dd77a71e3387328e01f9006e791f498be
Casey <caseystephenabbott@gmail.com>
2024-02-22 19:45:04 -1000

    made enrollment and escrip pages. all links work now except for what i did not make yet

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const showMenu = (toggleId, navId) => {
     const toggle = document.getElementById(toggleId),
-    nav = document.getElementById(navId)
+    nav = document.getElementById(navId);
 
     toggle.addEventListener('click', () => {
         nav.classList.toggle('show-menu')
@@ -15,5 +15,5 @@ cards.forEach(card => {
     card.addEventListener('click', () => {
         card.classList.toggle('flip_card')
     })
-});
+})
 
