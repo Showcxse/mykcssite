cea457f32c191d0aac371e1b3eb34504af428ffe
Casey <caseystephenabbott@gmail.com>
2024-03-07 19:22:16 -1000

    fixed the goddamn nav bar

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ const showMenu = (toggleId, navId) => {
     nav = document.getElementById(navId);
 
     toggle.addEventListener('click', () => {
-        nav.classList.toggle('show-menu')
-        toggle.classList.toggle('show-icon')
-    })
-}
-showMenu('nav-toggler', 'nav-menu');
+            nav.classList.toggle('show-menu');
+            nav.classList.toggle('show-icon');
+    
+        });
+    }
 
+showMenu('nav-toggler', 'nav-menu');
 
 const cards = document.querySelectorAll('.card');
 cards.forEach(card => {
@@ -21,12 +22,12 @@ document.addEventListener('DOMContentLoaded', () => {
     const observer = new IntersectionObserver((elements, observer) => {
         elements.forEach((element) => {
             if (element.isIntersecting) {
-                console.log("its working");
+                console.log("it's working delete me if you see this");
                 element.target.classList.add('visible');
                 observer.unobserve(element.target);
             }
         });
-    }, {threshold: 0.4});
+    }, {threshold: 0.35});
 
     const elementsToAnimate = document.querySelectorAll('[animated="true"]');
     elementsToAnimate.forEach((element) => {
